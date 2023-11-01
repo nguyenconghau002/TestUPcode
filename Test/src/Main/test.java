0b82fe702c612be5c06300f89f52ced80a2298ea
nguyenconghau002 <149589495+nguyenconghau002@users.noreply.github.com>
2023-11-01 19:43:05 +0700

    sua lan 2 hau

--- Test/src/Main/test.java
+++ Test/src/Main/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("hello group 6");
 		System.out.println("ung dung quan ly thu vien");
+		System.out.println("ggdgdgd");
 	}
 }
